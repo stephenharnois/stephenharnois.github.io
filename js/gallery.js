7f23a1d2ae3c90de2ae46b30a475ac59b7f21da1
Stephen Harnois <stephen.harnois@live.com>
2020-06-05 13:06:28 -0400

    Add JavaScript image gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage  = document.querySelector("#gallery-photo img");
+    let currentClass = "current";
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener("click", () => {
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            let newImageTitle = thumbnail.dataset.title;
+            let newImageDescription = thumbnail.dataset.description;
+            let newImagesAlt = thumbnail.alt;
+
+            mainImage.setAttribute("alt", newImagesAlt);
+            mainImage.setAttribute("src", newImageSrc);
+
+            document.querySelector(".current").classList.remove(currentClass);
+            thumbnail.parentNode.classList.add(currentClass);
+
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
+
+            title.innerHTML = newImageTitle;
+            description.innerHTML = newImageDescription;
+        })
+    })
 }
